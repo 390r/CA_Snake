5726d13ee0abb40d786314b0dc4ae90655741f33
390r <e.baklanov@innopolis.ru>
2018-03-17 17:03:07 +0300

    Add very first implementation of Turn Snake state mashine. Checked on online IDE for verilog on array implementation - it works.

--- snake.v
+++ snake.v
@@ -8,8 +8,21 @@ output segA, segB, segC, segD, segE, segF, segG, segDP;
   reg [7:0] arr [0:7] [0:1];		 //Positions of all segments of snake
   reg [1:0] refresh;				 //Refresh trigger
   reg [2:0] lastDirection = 2'b01;  //Last direction of head of snake
-  reg [2:0] turn1 = 2'b00;
+  reg [2:0] turn = 2'b00;
   reg [1:0] stepForward = 1'b0;		 //Trigger for next frame move
+  
+  always @(turn)
+    begin
+		if (turn == 2'b01) begin
+			if (lastDirection == 2'b00) begin
+				lastDirection = 2'b01;
+			end
+			else if (lastDirection == 2'b01) begin
+              $display("kok");
+				lastDirection = 2'b10;
+			end
+		end
+	 end
   
   always @(refresh)
     begin
@@ -30,18 +43,19 @@ output segA, segB, segC, segD, segE, segF, segG, segDP;
     begin
       case(lastDirection)
         2'b00: begin
-          posy = posy - 1;
+        posy = posy - 1;
 		  field[posy][posx] = 1'b0;
         end
-		2'b01: begin
+		  2'b01: begin
 		  posx = posx + 1;
 		  field[posy][posx] = 1'b0;
         end
-        2'b10: begin
+          2'b10: begin
+            $display("kek");
           posy = posy + 1;
 		  field[posy][posx] = 1'b0;
         end
-		2'b11: begin
+		  2'b11: begin
 		  posx = posx - 1;
 		  field[posy][posx] = 1'b0;
         end
@@ -50,18 +64,21 @@ output segA, segB, segC, segD, segE, segF, segG, segDP;
     end
   
 initial begin
-  #1 refresh = 1'b1;
   #1 field[0][0] = 1'b0;
   posx = 1'd0;
   posy = 1'd0;
- 
-  #1 refresh = 1'b0;
-  #1 stepForward = 1'b1;
   #1 refresh = 1'b1;
  
+  #1 stepForward = 1'b1;
+  #1 refresh = 1'b0;
+ 
+  #1 turn = 2'b01;
+ 
+  #1 stepForward = 1'b0;
+  #1 refresh = 1'b1;
 end
 
 
-endmodule
+endmodule          
 
 
